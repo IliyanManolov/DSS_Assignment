34a7269e303b30dec1c93fe77b5844d100a31a03
IliyanManolov <killerood1@gmail.com>
2023-05-01 20:37:58 +0300

    more ArticleController Tests

--- DSS_Assignment_Tests/ArticleControllerUnitTest.cs
+++ DSS_Assignment_Tests/ArticleControllerUnitTest.cs
@@ -26,35 +26,54 @@ namespace DSS_Assignment_Tests
 		[Fact]
 		public void AddArticle()
 		{
+			var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>()
+				.UseInMemoryDatabase(Guid.NewGuid().ToString());
+			var mockDbContext = new ApplicationDBContext(optionsBuilder.Options);
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
-            optionsBuilder.UseInMemoryDatabase("TestDB");
-            var mockDbContext = new Mock<ApplicationDBContext>(optionsBuilder.Options);
+			var mockUserRepository = new Mock<IUserRepository>();
+			mockUserRepository.Setup(repo => repo.GetUser("test", "testpass"))
+				.Returns(new User { Id = 1, Name = "test", Password = "testpass" });
+			var mockSession = new Mock<ISession>();
+
+			var mockArticleRepository = new Mock<IArticleRepository>();
+
+			var authController = new AuthController(mockUserRepository.Object);
+			var articleController = new ArticleController(mockDbContext, mockArticleRepository.Object);
+
+			authController.ControllerContext = new ControllerContext
+			{
+				HttpContext = new DefaultHttpContext { Session = mockSession.Object }
+			};
+
+			var usr = new User { Id = 1, Name = "test", Password = "testpass" };
+			authController.HttpContext.Session.SetInt32("ID", usr.Id);
+			var sesid = authController.HttpContext.Session.GetInt32("ID");
+
+			var article = new Article { Id = 1, Body = "testbody", CommentsAmount = 0, Image = "testimage", Title = "testtitle"};
+			var result = articleController.WriteArticle(article);
+			Assert.NotNull(mockDbContext.Articles);
+		}
+		[Fact]
+		public void AddEmptyArticle()
+		{
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var mockDbContext = new ApplicationDBContext(optionsBuilder.Options);
 
             var mockUserRepository = new Mock<IUserRepository>();
             mockUserRepository.Setup(repo => repo.GetUser("test", "testpass"))
                 .Returns(new User { Id = 1, Name = "test", Password = "testpass" });
-
-            var mockArticleRepository = new Mock<IArticleRepository>();
-            var article = new Article { Body = "testbody", Image = "testimage", Title = "testtitle", CommentsAmount = 0 };
-
             var mockSession = new Mock<ISession>();
 
+            var mockArticleRepository = new Mock<IArticleRepository>();
+
             var authController = new AuthController(mockUserRepository.Object);
-            var articleController = new ArticleController(mockDbContext.Object, mockArticleRepository.Object);
+            var articleController = new ArticleController(mockDbContext, mockArticleRepository.Object);
 
             authController.ControllerContext = new ControllerContext
             {
                 HttpContext = new DefaultHttpContext { Session = mockSession.Object }
             };
-            var usr = new User { Id = 1, Name = "test", Password = "testpass" };
-
-            authController.HttpContext.Session.SetInt32("ID", usr.Id);
-            var sesid = authController.HttpContext.Session.GetInt32("ID");
-            Assert.Equal(1, sesid);
-            var result = articleController.WriteArticle(article);
-
-            Assert.Equal(authController.HttpContext.Session.GetInt32("ID"), article.UserId);
         }
     }
 
